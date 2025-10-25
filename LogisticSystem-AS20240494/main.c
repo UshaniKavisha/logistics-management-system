dc9bed92f2db4e640f480ed84acfbde27adc596d
UshaniKavisha <as20240494@sci.sjp.ac.lk>
2025-10-25 15:29:44 +0530

    write customerCharge to deliveries.txt

--- LogisticSystem-AS20240494/main.c
+++ LogisticSystem-AS20240494/main.c
@@ -653,7 +653,7 @@ void saveDeliveries()
 
     for(int i=0;i<deliveryCount;i++)
     {
-        fprintf(filePointer,"%d,%d,%.2f,%d,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+        fprintf(filePointer,"%d,%d,%.2f,%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
             deliveries[i].sourceCity,
             deliveries[i].destinationCity,
             deliveries[i].weight,
@@ -662,6 +662,8 @@ void saveDeliveries()
             deliveries[i].deliveryCost,
             deliveries[i].fuelCost,
             deliveries[i].operationalCost,
-            deliveries[i].profit,);
+            deliveries[i].profit,
+            deliveries[i].customerCharge
+            );
     }
 }
